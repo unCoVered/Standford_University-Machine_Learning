59bdfd27c5a4c7c9977ad04f241b9ed1165ea574
Alex <alex134_16_zgz@hotmail.com>
2018-08-07 12:30:32 +0200

    moar

--- week3/machine-learning-ex2/ex2/costFunction.m
+++ week3/machine-learning-ex2/ex2/costFunction.m
@@ -21,9 +21,8 @@ grad = zeros(size(theta));
 %
 
 
-
-
-
+J = (1 / m) * sum( -y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta)) );
+grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
 
 
 
